9f081bcb39512dbe98e68155c8678d107207e350
ikrambahili <ikrambahili23@gmail.com>
2023-06-15 11:46:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
-#!/bin/bash
 #include<studio.h>
-{Programming is like building a multilingual puzzle 
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * return 0.
+ */
+
+int main (void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+
 }
-Return(0)

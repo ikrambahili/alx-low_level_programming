92cf4b356564e74296dfaa9a54e77f5b9ccc0e8a
ikrambahili <ikrambahili23@gmail.com>
2023-06-15 17:41:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - main point
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle
- * followed by a new line.
- * return: Always 0 (success)
+ *
+ * Description: A C program that prints with put function 
+ * 
+ * Return: Always 0 (success)
  */
 
 int main(void)

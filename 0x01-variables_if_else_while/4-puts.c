7711e65155370e8b0caf989d53782430dfaea5a5
ikrambahili <ikrambahili23@gmail.com>
2023-06-16 12:39:14 -0700

    4-puts.c

---
+++ 0x01-variables_if_else_while/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: A C program that prints a specific string using the puts function.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}

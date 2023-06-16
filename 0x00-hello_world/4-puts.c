d319245114112c79f11ad21bbf4a68ae6418fed3
ikrambahili <ikrambahili23@gmail.com>
2023-06-15 17:27:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- * retur: Always 0 (success)
+ * main - main point
+ * Write a C program that prints exactly "Programming is like building a multilingual puzzle
+ * followed by a new line.
+ * return: Always 0 (success)
  */
 
 int main(void)

19461bd79d90d6aab333a0de21f291201cdd1360
ikrambahili <ikrambahili23@gmail.com>
2023-06-16 08:15:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
 #include<stdio.h>
 
-/**
- * main - main point
- *
- * Description: A C program that prints with put function 
- * 
- * Return: Always 0 (success)
- */
-
-int main(void)
-
+/*
+* main - main point 
+* Description: A C program that prints with put function 
+*
+* Return: Always 0 -succcess) 
+*/
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
-

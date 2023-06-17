5312153d23b58f2fdabccfe047fe6e1c8cf13bee
ikrambahili <ikrambahili23@gmail.com>
2023-06-17 16:47:04 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: A C program that prints the lowercase alphabet excluding 'q' and 'e' using putchar.
- *
- * Return: Always 0 (success)
- */
+* main - Entry point of the program
+*
+* Description: A C program that prints the lowercase
+* alphabet excluding 'q' and 'e' using putchar.
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'q' && letter != 'e')
-            putchar(letter);
-    }
-
-    putchar('\n');
-
-    return(0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+putchar(letter);
 }
-
+putchar ('\n');
+return (0);
+}

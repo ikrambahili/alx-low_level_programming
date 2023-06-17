65392e855765d9354462efbef41d6d1aea4f9ba0
ikrambahili <ikrambahili23@gmail.com>
2023-06-16 18:01:09 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: A C program that prints all possible different combinations of two digits using putchar.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+
+	 putchar('0');
+    putchar('1');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('2');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('3');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('4');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('5');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('6');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('7');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('8');
+    putchar(',');
+    putchar(' ');
+
+    putchar('0');
+    putchar('9');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('2');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('3');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('4');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('5');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('6');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('7');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('8');
+    putchar(',');
+    putchar(' ');
+
+    putchar('1');
+    putchar('9');
+    putchar(',');
+    putchar(' ');
+
+    putchar('2');
+    putchar('3');
+    putchar(',');
+    putchar(' ');
+
+    putchar('2');
+    putchar('4');
+    putchar(',');
+    putchar(' ');
+
+    putchar('2');
+    putchar('5');
+    putchar(',');
+    putchar(' ');
+
+    putchar('2');
+    putchar('6');
+    putchar(',');
+    putchar(' ');
+
+    putchar('2');
+    putchar('7');
+    putchar(',');
+    putchar(' ');
+
+    putchar('2');
+    putchar('8');
+    putchar(',');
+    putchar(' ');
+
+    putchar('2');
+    putchar('9');
+    putchar(',');
+    putchar(' ');
+
+    putchar('3');
+    putchar('4');
+    putchar(',');
+    putchar(' ');
+
+    putchar('3');
+    putchar('5');
+    putchar(',');
+    putchar(' ');
+
+    putchar('3');
+    putchar('6');
+    putchar(',');
+    putchar(' ');
+
+    putchar('3');
+    putchar('7');
+    putchar(',');
+    putchar(' ');
+
+    putchar('3');
+    putchar('8');
+    putchar(',');
+    putchar(' ');
+
+    putchar('3');
+    putchar('9');
+    putchar(',');
+    putchar(' ');
+
+    putchar('4');
+    putchar('5');
+    putchar(',');
+    putchar(' ');
+
+    putchar('4');
+    putchar('6');
+    putchar(',');
+    putchar(' ');
+
+    putchar('4');
+    putchar('7');
+    putchar(',');
+    putchar(' ');
+
+    putchar('4');
+    putchar('8');
+    putchar(',');
+    putchar(' ');
+
+    putchar('4');
+    putchar('9');
+    putchar(',');
+    putchar(' ');
+
+    putchar('5');
+    putchar('6');
+    putchar(',');
+    putchar(' ');
+
+    putchar('5');
+    putchar('7');
+    putchar(',');
+    putchar(' ');
+
+    putchar('5');
+    putchar('8');
+    putchar(',');
+    putchar(' ');
+
+    putchar('5');
+    putchar('9');
+    putchar(',');
+    putchar(' ');
+
+    putchar('6');
+    putchar('7');
+    putchar(',');
+    putchar(' ');
+
+    putchar('6');
+
+
+
+
+

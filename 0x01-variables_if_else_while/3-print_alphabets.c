30149e233bc924ddfd1a3f449e2be569c7118d48
ikrambahili <ikrambahili23@gmail.com>
2023-06-17 16:37:54 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: A C program that prints the lowercase and uppercase alphabet using putchar.
- *
- * Return: Always 0 (success)
- */
+* main - Entry point of the program
+*
+* Description: A C program that prints the lowercase and
+* uppercase alphabet using putchar.
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-    char lowercase, uppercase;
-
-    for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-        putchar(lowercase);
-
-    for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-        putchar(uppercase);
-
-    putchar('\n');
-
-    return (0);
+char lowercase, uppercase;
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+putchar(lowercase);
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+putchar(uppercase);
+putchar('\n');
+return (0);
 }
 

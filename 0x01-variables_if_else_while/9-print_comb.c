dfbc0b3b904c6b51b4190329248f4eb38db5b842
ikrambahili <ikrambahili23@gmail.com>
2023-06-17 17:27:32 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,25 +5,25 @@
 #include <time.h>
 
 /**
- * main - Entry point
- * Description: A C program that prints all possible combinations of single-digit numbers using putchar.
- * return: Always 0 (success)
- */
+* main - Entry point
+* Description: A C program that prints all possible
+* combinations of single-digit numbers using putchar.
+* Return: Always 0 (success)
+*/
 
-int main (void)
+int main(void)
 
 {
-      int d;
-       
-       for (d= '0'; d <= '9'; d++)
-       {
-       putchar (d);
-       if (d != '9')
-       {
-       putchar(',');
-       putchar(' '); 	 
-       }		
-       }      
-       putchar('\n');
-       return (0);
- }
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar (d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

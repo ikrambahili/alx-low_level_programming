3f18155be865553704c485b57cb2e90a9f6f742d
ikrambahili <ikrambahili23@gmail.com>
2023-06-17 17:12:14 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: A C program that prints the lowercase alphabet in reverse using putchar.
- *
- * Return: Always 0 (success)
- */
+* main - Entry point of the program
+*
+* Description: A C program that prints the lowercase
+* alphabet in reverse using putchar.
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-    char letter;
-
-    for (letter = 'z'; letter >= 'a'; letter--)
-        putchar(letter);
-
-    putchar('\n');
-
-    return 0;
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
 }
-

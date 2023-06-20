dc93fee1694eb6d82b6ca1d949030da2194b0a18
ikrambahili <ikrambahili23@gmail.com>
2023-06-20 13:33:48 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
 */
@@ -6,6 +7,7 @@ void print_alphabet_x10(void)
 {
 int i;
 char letter;
+
 for (i = 0; i < 10; i++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
@@ -13,5 +15,7 @@ for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 }
 }
+
 _putchar('\n');
 }
+

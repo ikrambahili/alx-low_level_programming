f007222c04bc0c48aff7333a48431b635d19ef3d
ikrambahili <ikrambahili23@gmail.com>
2023-08-01 17:30:27 -0700

    103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,34 +1,40 @@
 #include "lists.h"
 
 /**
-* find_listint_loop - Finds the loop in a linked list
-* @head: Pointer to the first element of the list
-*
-* Return: The address of the node where the loop starts, 
-* or NULL if there is no loop
-*/
+ * find_listint_loop - Finds the loop in a linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The address of the node where the loop starts, or NULL if there is no loop.
+ */
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *slow, *fast;
-if (head == NULL || head->next == NULL)
-return (NULL);
-slow = head;
-fast = head;
-while (fast && fast->next)
-{
-slow = slow->next;
-fast = fast->next->next;
-if (slow == fast)
-{
-/* Loop detected */
-slow = head;
-while (slow != fast)
-{
-slow = slow->next;
-fast = fast->next;
-return (slow);
-}
-}
-return (NULL); /* No loop found */
+	listint_t *slow, *fast;
+
+	if (head == NULL)
+		return (NULL);
+
+	slow = head;
+	fast = head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			/* Loop detected, reset one of the pointers and find the start of the loop */
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			return (slow);
+		}
+	}
+
+	return (NULL);
 }
 
